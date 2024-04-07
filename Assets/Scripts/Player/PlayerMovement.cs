5e438317cd83a74bf9b2fe83308e88d595cd2e18
Aegistus <joe.ballanders@gmail.com>
2024-04-06 20:48:44 -0400

    Add Player Dive

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -17,63 +17,101 @@ public class PlayerMovement : AgentMovement
     PlayerController playerController;
     Vector3 moveVector;
 
+    Rigidbody rb;
+    bool diving = false;
+    float divingTime = 3f;
+    float diveTimer;
+    float diveForce = 400f;
+    Vector3 diveForcePosition = new Vector3(0, 1.5f, 0);
+
     void Awake()
     {
         cameraTransform = FindObjectOfType<CameraController>().transform;
         charController = GetComponent<CharacterController>();
         playerController = GetComponent<PlayerController>();
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        // lateral movement
-        moveVector = Vector3.zero;
-        if (Input.GetKey(KeyCode.W))
+        // diving
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
         {
-            moveVector += cameraTransform.forward;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            moveVector -= cameraTransform.forward;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            moveVector -= cameraTransform.right;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            moveVector += cameraTransform.right;
-        }
-        moveVector.y = 0;
-        moveVector = moveVector.normalized;
-        //moveVector = transform.TransformDirection(moveVector);
-        charController.Move(moveVector * moveSpeed * Time.deltaTime);
-
-        RaycastHit rayHit;
-        if (Physics.Raycast(transform.position, Vector3.down, out rayHit, 20f, groundLayer))
-        {
-            transform.position = rayHit.point;
-        }
-
-        // rotational movement
-        if (inCombatMode)
-        {
-            if (moveVector.sqrMagnitude > 0 && !playerController.Aim)
+            if (!diving)
             {
-                transform.LookAt(transform.position + moveVector);
-                torso.localRotation = Quaternion.identity;
+                diving = true;
+                charController.enabled = false;
+                rb.useGravity = true;
+                rb.isKinematic = false;
+                rb.constraints = 0;
+                rb.AddForceAtPosition(transform.forward * diveForce, transform.position + diveForcePosition);
+                rb.AddForceAtPosition(transform.up * diveForce * .5f, transform.position + diveForcePosition);
+                diveTimer = divingTime;
+            }
+        }
+        if (diving)
+        {
+            diveTimer -= Time.deltaTime;
+            if (diveTimer <= 0)
+            {
+                diving = false;
+                charController.enabled = true;
+                rb.useGravity = true;
+                rb.isKinematic = true;
+                rb.constraints = RigidbodyConstraints.FreezeRotation;
+            }
+        }
+        if (!diving)
+        {
+            // lateral movement
+            moveVector = Vector3.zero;
+            if (Input.GetKey(KeyCode.W))
+            {
+                moveVector += cameraTransform.forward;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                moveVector -= cameraTransform.forward;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                moveVector -= cameraTransform.right;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                moveVector += cameraTransform.right;
+            }
+            moveVector.y = 0;
+            moveVector = moveVector.normalized;
+            //moveVector = transform.TransformDirection(moveVector);
+            charController.Move(moveVector * moveSpeed * Time.deltaTime);
+
+            RaycastHit rayHit;
+            if (Physics.Raycast(transform.position, Vector3.down, out rayHit, 20f, groundLayer))
+            {
+                transform.position = rayHit.point;
+            }
+
+            // rotational movement
+            if (inCombatMode)
+            {
+                if (moveVector.sqrMagnitude > 0 && !playerController.Aim)
+                {
+                    transform.LookAt(transform.position + moveVector);
+                    torso.localRotation = Quaternion.identity;
+                }
+                else
+                {
+                    transform.rotation = Quaternion.LookRotation(cameraTransform.forward, Vector3.up);
+                    transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+                    torso.rotation = cameraTransform.rotation;
+                    legs.localRotation = Quaternion.identity;
+                }
             }
             else
             {
-                transform.rotation = Quaternion.LookRotation(cameraTransform.forward, Vector3.up);
-                transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
-                torso.rotation = cameraTransform.rotation;
-                legs.localRotation = Quaternion.identity;
+                torso.localRotation = Quaternion.identity;
             }
-        }
-        else
-        {
-            torso.localRotation = Quaternion.identity;
         }
 
         // fix falling through map
